a134df790c28ae6d677abc8bb7d4cb1c63213dd1
Jack-M1 <114680493+Jack-M1@users.noreply.github.com>
2024-04-06 15:26:32 +0100

    Removed old unneeded code

--- main.py
+++ main.py
@@ -4,59 +4,6 @@ import matplotlib.pyplot as plt
 from dask import dataframe as dd
 import time
 
-
-datasetTbD = pd.read_csv("Trips_by_Distance.csv")
-datasetFullTrips = pd.read_csv("Trips_Full Data.csv")
-#date = "1/1/2019"
-level = "National"
-#print(dataset.loc["National","01/01/2019", "Population Staying at Home"])
-#print(dataset.loc["Level"=="National", "Population Staying at Home"])
-
-#levelFilter = dataset.query("Population Staying at Home < 7000000")
-levelFilter = datasetTbD.loc[datasetTbD["Level"]=="National"]
-levelFilter2 = levelFilter.loc[levelFilter["Date"] == "1/1/2019"]
-#print(levelFilter2.iloc[0,6])
-
-
-#month, day, year = date.split('/')
-#day = int(day) 
-
-#include code for grabbing value of first date here
-
-
-# filter = datasetTbD.loc[datasetTbD["Date"]==date]
-# value = filter.iloc[0,6]
-# int(value)
-# for i in range(6): # Stays as 6 as only counts for the succeeding week after the date
-#     day += 1 # Increments the day in the date to gain values for the week
-#     print(day)
-#     newDate = f"{month}/{day}/{year}"
-#     filter = datasetTbD.loc[datasetTbD["Date"]==newDate]
-#     tempValue = filter.iloc[0,6]
-#     int(tempValue)
-#     value += tempValue
-#     print(round(value/7)) # Round the existing value and produce the average of the week
-
-
-# datasetTbD['Date'] = pd.to_datetime(datasetTbD['Date'])
-# levelFilter = datasetTbD.loc[datasetTbD["Level"]=="National"]
-# levelFilter = levelFilter.groupby(pd.Grouper(key = 'Date', freq = 'W'))
-# levelFilter = levelFilter["Population Staying at Home"].mean().round().reset_index()
-#print(levelFilter)
-
-#value = datasetFullTrips.iloc[0,10]
-#print(datasetFullTrips["Trips"])
-value = datasetFullTrips["Trips 1-25 Miles"] + datasetFullTrips["Trips 1-3 Miles"] + datasetFullTrips["Trips 10-25 Miles"] + datasetFullTrips["Trips 100-250 Miles"] + datasetFullTrips["Trips 100+ Miles"] + datasetFullTrips["Trips 25-100 Miles"] + datasetFullTrips["Trips 25-50 Miles"] + datasetFullTrips["Trips 250-500 Miles"] + datasetFullTrips["Trips 3-5 Miles"] + datasetFullTrips["Trips 5-10 Miles"] + datasetFullTrips["Trips 50-100 Miles"] + datasetFullTrips["Trips 500+ Miles"]
-#datasetFullTripsAvg = datasetFullTrips.groupby("Week of Date")[["Trips 1-3 Miles"]].mean()
-#print(datasetFullTripsAvg)
-
-# Trips for 1-25 and others are people conducting those trips, not amount of trips
-# result = 0
-# for i in range(7):  
-#      temp = value.iloc[i]
-#      result = result + temp
-# print(value)
-# print(result)
 def question1A_Part1():
     start_time=time.time()
     dataset = pd.read_csv("Trips_by_Distance.csv")
@@ -79,6 +26,7 @@ def question1A_Part1():
     plt.xlabel("Weeks Over Dataset")
     plt.ylabel("Population Staying at Home")
     plt.xticks(rotation=80)
+    plt.title("Question 1A Part 1")
     plt.tight_layout()
     plt.show()
 
@@ -97,6 +45,7 @@ def question1A_Part2():
     plt.xlabel("Distance Groups")
     plt.ylabel("Average Number of Trips")
     plt.xticks(rotation=80)
+    plt.title("Question 1A Part 2")
     plt.tight_layout()
     plt.show()
 
@@ -121,6 +70,7 @@ def question1B():
     plt.xlabel("Date")
     plt.ylabel("Number of Trips")
     plt.xticks(rotation=80)
+    plt.title("Question 1B")
     plt.tight_layout()
     plt.show()
 
@@ -208,6 +158,7 @@ def question1E():
     plt.xlabel("Date")
     plt.ylabel("Number of Travelers")
     plt.xticks(range(len(datetime)), datetime, rotation=45)
+    plt.title("Question 1E")
     plt.legend(title="Trip Distance", loc="upper right")
     plt.tight_layout()
     plt.show()
